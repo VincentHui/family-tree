542c54ccf5aacbb643c5fafa35d1284814ddda68
Vincent Hui <vincenthui@theambassadors.com>
2020-06-22 00:01:03 +0100

    adding preloader logic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const authenticate = ()=>{
+  return new Promise(resolve => setTimeout(resolve, 2000)) // 2 seconds
+}
+
 function App() {
+  useEffect(() => {
+    // code to run on component mount
+    authenticate().then(() => {
+      const ele = document.getElementById('ipl-progress-indicator')
+      if(ele){
+        // fade out
+        ele.classList.add('available')
+        setTimeout(() => {
+          // remove from DOM
+          ele.outerHTML = ''
+        }, 2000)
+      }
+    })
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
